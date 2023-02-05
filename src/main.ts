957fcde8532e9629f6ee94f5f026899fdfd3ad6c
Erhuero <dsimgen.labinnovation@gmail.com>
2023-02-05 22:25:21 +0100

    message

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ WA.onInit().then(() => {
         currentPopup = WA.ui.openPopup("clockPopup", "Il est : " + time, []);
     })
 //trouver le chemin relatif des tilesets
-currentPopup = WA.ui.openPopup("welcomePopup", `<img src="https://goopics.net/i/sc0gps" alt="Welcome image">`, []);
+currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au salon TTS, dirigez-vous avec des panneaux", []);
 
 
    // currentPopup = WA.ui.openPopup("welcomePopup", `<img src="" alt="Welcome image">`, []);
